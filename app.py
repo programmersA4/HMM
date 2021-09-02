1e99031307884866da34fd34f0a3ee89cdb18042
JJ-HH <docessonhos@naver.com>
2021-09-02 15:33:48 +0900

    partial update

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, json, render_template, url_for, request, redirect, jsonify, make_response
+from flask_bootstrap import Bootstrap
+from flask_cors import CORS, cross_origin
+import service
+
+
+app = Flask(__name__, template_folder='Template')
+cors = CORS(app)
+Bootstrap(app)
+
+"""
+Routes
+"""
+@app.route('/', methods=['GET'])
+def index():
+    # return render_template('webcam.html')
+    return render_template('main_page.html')
+
+@app.route('/capture_img', methods=['POST'])
+def capture_img():
+    msg = service.save_img(request.form["img"])
+    return make_response(msg)
+
+
+if __name__ == "__main__":
+    app.run()
