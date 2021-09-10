aeb5094a69ccd0d58fd923beec35b5c7c11323e1
JJ-HH <docessonhos@naver.com>
2021-09-10 17:47:11 +0900

    change webpage layout

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, json, render_template, url_for, request, redirect, jsonify, make_response
 from flask_bootstrap import Bootstrap
 from flask_cors import CORS, cross_origin
+import torch
+
 
 import service
 from preprocess import preprocess, predict
-
+from pred import get_pred
 
 
 app = Flask(__name__, template_folder='Template')
 cors = CORS(app)
 Bootstrap(app)
+
+model = torch.hub.load('JJ-HH/yolov5', 'yolov5x')
 
 """
 Routes
@@ -22,12 +26,14 @@ def index():
 @app.route('/capture_img', methods=['POST'])
 def capture_img():
     msg, im_file = service.save_img(request.form["img"])
-    print(msg, im_file)
-    rib = preprocess(im_file[2:])
-    res = predict(rib)
-    print(res)
-    # return make_response(*res)
-    return make_response({'result': res})
+    # rib = preprocess(im_file[2:])
+    # res = predict(rib)
+    # print(res)
+    print(msg, im_file[2:])
+    res = model(im_file[2:])
+    l = get_pred(res)
+    print(l)
+    return make_response({'result': l})
 
 @app.route('/sample', methods=['GET'])
 def sample_api_response():
