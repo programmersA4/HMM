188baf10d77e59269d2124cf25e2ca96608e8550
Ubuntu <ubuntu@ip-172-31-42-61.us-east-2.compute.internal>
2021-09-15 09:23:59 +0000

    auth process modal hide added

--- static/src/main.js
+++ static/src/main.js
@@ -33,21 +33,38 @@ function sendImg(img_base64){
   formbody.append('challenge', localStorage.getItem('challenge'))
   
   // xhr.open('POST', 'http://localhost:5000/capture_img', true);
-  xhr.open('POST', 'https://ec2-18-216-118-227.us-east-2.compute.amazonaws.com/capture_img', true);
+  xhr.open('POST', '//ec2-18-216-118-227.us-east-2.compute.amazonaws.com/capture_img', true);
   xhr.onload = () => {
       var data = JSON.parse(xhr.responseText);
       console.log(data['success']);
       var bcolor = 'danger'
+      var alert1 = 'danger'
+      var alert2 = 'Danger'
+      var notice = "재인증이 필요합니다."
       if (data['success'] === true) {
         bcolor = 'success'
+        alert1 = 'success'
+        alert2 = 'Success'
+        notice = '인증이 완료되었습니다.'
       }
       localStorage.setItem('img', data['img']);
       document.querySelector('#canvas-wrapper').innerHTML = 
-      // `<img src="http://localhost:5000/image/${localStorage.getItem('img')}" 
-      `<img src="https://ec2-18-216-118-227.us-east-2.compute.amazonaws.com/image/${data['img']}" 
+      `
+      <div class="alert alert-${alert1} d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="${alert2}:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+        <div>
+          ${notice}
+        </div>
+      </div>
+
+      <img src="//ec2-18-216-118-227.us-east-2.compute.amazonaws.com/image/${data['img']}" 
         class="mt-1 border border-5 border-${bcolor} rounded px-0" width="640" height="480"
         >
-      </img>`
+      </img>`;
+
+      if (data['success'] === true){
+        setTimeout(() => $( "#captureModal" ).modal('hide'), 3300);
+      }
     };
   xhr.send(formbody);
 }
